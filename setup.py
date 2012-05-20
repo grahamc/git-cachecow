515f9dc924e6c117e21ba76d33e77e485a271792
Graham Christensen <gchristensen@nationalfield.org>
2012-05-20 17:45:42 -0400

    Describing the project more, for clarity.

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ from distutils.core import setup
 
 setup(name='git-cachecow',
         version='0.1',
+        url='https://github.com/grahamc/git-cachecow',
+        author='Graham Christensen',
+        author_email='graham@grahamc.com',
+        description='Cache git clone and git submodules in a cache directory.',
         py_modules=[
-            'backwards',
+            'backwards'
+        ],
+        scripts=[
             'git-cclone',
             'git-scclone',
             'git-submodule-describe',
